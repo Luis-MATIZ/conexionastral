b07ceb1bdf39e1068b2f0611e26f93b6648caffe
Luis Rafael Matiz López <149787952+Luis-MATIZ@users.noreply.github.com>
2026-03-15 23:42:59 +0100

    Mejoramos la interfaz del BottomNavigationBar #9

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  //static Color primary = Colors.deepPurple;
+  static Color primary = Color(0xFF2a5298);
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
     //Todo: Crearemos todo el tema de la APP aquí.
--- lib/widgets/custom_navigationbar.dart
+++ lib/widgets/custom_navigationbar.dart
@@ -11,13 +11,22 @@ class CustomNavigationBar extends StatelessWidget {
     final currentIndex = uiProvider.selectedMenuOpt;
 
     return BottomNavigationBar(
+      backgroundColor: Color(0xFF2a5298),
       onTap: (int i) => uiProvider.selectedMenuOpt = i,
       currentIndex: currentIndex,
+      unselectedItemColor: Colors.white,
+      selectedItemColor: Colors.white,
       items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: 'Consejos'),
         BottomNavigationBarItem(
-          icon: Icon(Icons.self_improvement),
+          icon: Icon(Icons.home, color: Colors.white),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.menu_book, color: Colors.white),
+          label: 'Consejos',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.self_improvement, color: Colors.white),
           label: 'Rituales',
         ),
       ],
